8b293fa35cb8379158bf3bd965245ca547381c64
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-02-12 21:24:43 -0600

    start scripts

---
+++ scripts/classroom-list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from .scopes import SCOPES
+
+print(SCOPES)
