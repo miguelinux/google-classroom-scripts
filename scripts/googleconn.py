3d4db6ce4d37cadb45c0783cbc9e47ccdbf1bf79
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-02-20 21:05:33 -0600

    add decouple

--- scripts/googleconn.py
+++ scripts/googleconn.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import os.path
 
+from decouple import config
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -16,19 +17,21 @@ def conectar():
     Prints the names of the first 10 courses the user has access to.
     """
     creds = None
+    g_token = config("GOOGLE_TOKEN", default="token.json")
+    g_credentials = config("GOOGLE_CREDENTIALS", default="credentials.json")
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(g_token):
+        creds = Credentials.from_authorized_user_file(g_token, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(g_credentials, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open(g_token, "w") as token:
             token.write(creds.to_json())
     return creds
